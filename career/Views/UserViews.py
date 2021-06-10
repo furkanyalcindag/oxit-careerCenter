2716fd116c49540297f6eb47f2ab33c446d16efc
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2021-06-10 13:51:43 +0300

    bug fix

--- career/Views/UserViews.py
+++ career/Views/UserViews.py
@@ -14,8 +14,25 @@ class UserAPI(APIView):
     def get(self, request, format=None):
         users = User.objects.exclude(groups__name__in=['Admin', 'Consultant', 'Company', 'Student'])
 
-        arr = []
-        for user in users:
+        if request.GET.get('id') is not None:
+            arr = []
+            for user in users:
+                api_data = dict()
+                api_data['firstName'] = user.first_name
+                api_data['lastName'] = user.last_name
+                api_data['email'] = user.email
+                api_data['id'] = user.id
+
+                api_data['groupName'] = Group.objects.get(user=user).name
+
+                arr.append(api_data)
+
+            serializers = UserSerializer(arr, many=True, context={'request': request})
+
+            return Response(serializers.data, status=status.HTTP_200_OK)
+        else:
+            user = User.objects.get(~Q(groups__name__in=['Admin', 'Consultant', 'Company', 'Student']),
+                                    id=int(request.GET.get('id')))
             api_data = dict()
             api_data['firstName'] = user.first_name
             api_data['lastName'] = user.last_name
@@ -24,11 +41,9 @@ class UserAPI(APIView):
 
             api_data['groupName'] = Group.objects.get(user=user).name
 
-            arr.append(api_data)
+            serializers = UserSerializer(api_data, context={'request': request})
 
-        serializers = UserSerializer(arr, many=True, context={'request': request})
-
-        return Response(serializers.data, status=status.HTTP_200_OK)
+            return Response(serializers.data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         try:
@@ -93,6 +108,20 @@ class GroupAPI(APIView):
 
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        except:
+            traceback.print_exc()
+            return Response("error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def put(self, request, format=None):
+        try:
+            instance = Group.objects.get(id=request.GET.get('id'))
+            serializer = GroupSerializer(data=request.data, instance=instance,
+                                         context={'request': request})
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message": "group is updated"}, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except:
             traceback.print_exc()
             return Response("error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
