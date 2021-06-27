98c1907e69d6fc60ca9d3b72d87dc47bd3ffa4ee
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2021-06-27 17:28:11 +0300

    bf

--- career/Views/StudentViews.py
+++ career/Views/StudentViews.py
@@ -1,3 +1,6 @@
+import os
+import platform
+import subprocess
 import traceback
 
 import pdfkit
@@ -1249,6 +1252,21 @@ class StudentDriverLicenseApi(APIView):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+def _get_pdfkit_config():
+    """wkhtmltopdf lives and functions differently depending on Windows or Linux. We
+     need to support both since we develop on windows but deploy on Heroku.
+
+    Returns:
+        A pdfkit configuration
+    """
+    if platform.system() == 'Windows':
+        return pdfkit.configuration(
+            wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
+    else:
+        WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')],
+                                           stdout=subprocess.PIPE).communicate()[0].strip()
+        return pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+
 class StudentCVExportPDFApi(APIView):
     permission_classes = (IsAuthenticated,)
 
@@ -1313,7 +1331,7 @@ class StudentCVExportPDFApi(APIView):
             options = {
                 "enable-local-file-access": None
             }
-            output = pdfkit.from_string(html, output_path=False, options=options)
+            output = pdfkit.from_string(html, output_path=False, configuration=_get_pdfkit_config(), options=options)
             response = HttpResponse(content_type="application/pdf")
             response.write(output)
 
