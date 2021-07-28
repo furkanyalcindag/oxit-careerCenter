0f406034393da444615ccc39881891f5ac864f09
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2021-07-28 12:02:48 +0300

    bug fix

--- career/models/Department.py
+++ career/models/Department.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-from career.models.BaseModel import BaseModel
 
-
-class Department(BaseModel):
+class Department(models.Model):
     name = models.CharField(max_length=256, null=True, blank=True)
-
