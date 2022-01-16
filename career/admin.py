5c85590c7bfd3554c53118299b5b1c63502f7985
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2022-01-17 00:29:42 +0300

    init

--- career/admin.py
+++ career/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
+from career.models.Profile import Profile
 from career.models.Company import Company
 from career.models.Student import Student
 
 admin.site.register(Company)
 admin.site.register(Student)
+admin.site.register(Profile)
